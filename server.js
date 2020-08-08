b462ada13b906dec1facac9cb12f7eb9e6753ccf
Wilson <wilsonbirch@gmail.com>
2020-08-08 10:58:02 -0400

    removed comments

--- server.js
+++ server.js
@@ -33,27 +33,26 @@ app.get("/", function(req, res) {
   res.json(path.join(__dirname, "index.html"));
 });
 
- //Displays all notes
-//app.get("/api/notes", function(req, res) {
-  //let rawNotes = fs.readFileSync("db/db.json");
-  //let finalNotes = JSON.parse(rawNotes);
-  //return res.json(finalNotes);
-//});
+//Displays all notes
+app.get("/api/notes", function(req, res) {
+  let rawNotes = fs.readFileSync("db/db.json");
+  let finalNotes = JSON.parse(rawNotes);
+  return res.json(finalNotes);
+});
 
-//app.post("/api/notes", function(req, res) {
-  //let rawNotes = fs.readFileSync("db/db.json");
-  //let newNote = req.body;
+app.post("/api/notes", function(req, res) {
+  let rawNotes = fs.readFileSync("db/db.json");
+  let newNote = req.body;
 
-  // Using a RegEx Pattern to remove spaces from newNote
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  //newNote.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+   //Using a RegEx Pattern to remove spaces from newNote
+  newNote.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
 
-  //console.log(newNote);
+  console.log(newNote);
 
-  //rawnotes.push(newNote);
+  rawnotes.push(newNote);
 
-  //res.json(newNote);
-//});
+  res.json(newNote);
+});
 
 //starts the server to begin listening
 app.listen(PORT, function() {
